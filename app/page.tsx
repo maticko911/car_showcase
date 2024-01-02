02fce5add4be15f3d1e07cf65b2a5b0f24656b65
maticko911 <lozinsekmatic1@gmail.com>
2024-01-02 10:33:00 +0100

    first-commit

---
+++ app/page.tsx
@@ -0,0 +1,106 @@
+"use client";
+
+import Image from "next/image";
+
+import { CustomFilter, Hero, SearchBar, CarCard, ShowMore } from "@/components";
+import { fetchCars } from "@/utils";
+import { fuels, yearsOfProduction } from "@/constants";
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const [allCars, setAllCars] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  //search state
+  const [manufacture, setManufacture] = useState("");
+  const [model, setModel] = useState("");
+
+  //filter state
+  const [fuel, setFuel] = useState("");
+  const [year, setYear] = useState(2022);
+
+  //pagination state
+  const [limit, setLimit] = useState(10);
+
+  const getCars = async () => {
+    setLoading(true);
+    try {
+      const result = await fetchCars({
+        manufacturer: manufacture || "",
+        year: year || 2022,
+        fuel: fuel || "",
+        limit: limit || 10,
+        model: model || "",
+      });
+
+      setAllCars(result);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getCars();
+  }, [fuel, year, limit, manufacture, model]);
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
+  return (
+    <main className="overflow-hidden">
+      <Hero />
+
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+          <p>Explore the cars you might like</p>
+        </div>
+
+        <div className="home__filters">
+          <SearchBar setManufacture={setManufacture} setModel={setModel} />
+
+          <div className="home__filter-container">
+            <CustomFilter title="Fuel" options={fuels} setFilter={setFuel} />
+            <CustomFilter
+              title="Year"
+              options={yearsOfProduction}
+              setFilter={setYear}
+            />
+          </div>
+        </div>
+
+        {allCars.length > 0 ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
+            {loading && (
+              <div className="mt-16 w-full flex-center">
+                <Image
+                  src="/loader.svg"
+                  alt="loader"
+                  height={50}
+                  width={50}
+                  className="object-contain"
+                />
+              </div>
+            )}
+            <ShowMore
+              pageNumber={limit / 10}
+              isNext={limit > allCars.length}
+              setLimit={setLimit}
+            />
+          </section>
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">Oops, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
+      </div>
+    </main>
+  );
+}
